7f849c2a871a0c86637eb094d61f8e5f2fe0521f
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-02-04 19:45:45 -0300

    Criando uma calculadora simples

--- app/src/main/java/uanderson/com/br/curso/android_studio/MainActivity.java
+++ app/src/main/java/uanderson/com/br/curso/android_studio/MainActivity.java
@@ -2,6 +2,7 @@ package uanderson.com.br.curso.android_studio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,23 +10,60 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    double num1 = 0, num2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//Relaciona o código a view/tela
 
-        // Associando os elementos da view ao código java que irá manipular eles.
-        Button btn = (Button) findViewById(R.id.button);
-        TextView tv_nome = (TextView) findViewById(R.id.tv_nome);
-        EditText et_nome = (EditText) findViewById(R.id.et_nome);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        // Associando os elementos da view ao código java que irá manipular eles.
+        Button btn_somar = (Button) findViewById(R.id.btn_soma);
+        Button btn_subtrair = (Button) findViewById(R.id.btn_subtrai);
+        Button btn_multiplicar = (Button) findViewById(R.id.btn_multiplica);
+        Button btn_dividir = (Button) findViewById(R.id.btn_divide);
+
+        TextView tv_resultado = (TextView) findViewById(R.id.tv_resultado);
+
+        EditText et_valor1 = (EditText) findViewById(R.id.et_valor1);
+        EditText et_valor2 = (EditText) findViewById(R.id.et_valor2);
+
+        btn_somar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            @SuppressLint("DefaultLocale")
+            public void onClick(View v) {
+                num1 = Double.parseDouble(et_valor1.getText().toString());
+                num2 = Double.parseDouble(et_valor2.getText().toString());
+                tv_resultado.setText(String.format("%.2f", (num1 + num2)));
+            }
+        });
+        btn_subtrair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            @SuppressLint("DefaultLocale")
+            public void onClick(View v) {
+                num1 = Double.parseDouble(et_valor1.getText().toString());
+                num2 = Double.parseDouble(et_valor2.getText().toString());
+                tv_resultado.setText(String.format("%.2f", (num1 - num2)));
+            }
+        });
+        btn_multiplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            @SuppressLint("DefaultLocale")
+            public void onClick(View v) {
+                num1 = Double.parseDouble(et_valor1.getText().toString());
+                num2 = Double.parseDouble(et_valor2.getText().toString());
+                tv_resultado.setText(String.format("%.2f", (num1 * num2)));
+
+            }
+        });
+        btn_dividir.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("DefaultLocale")
             @Override
             public void onClick(View v) {
-                // Pegando o conteúdo do editText e passando para o TextView
-                String nome = et_nome.getText().toString();
-                tv_nome.setText(nome);
+                num1 = Double.parseDouble(et_valor1.getText().toString());
+                num2 = Double.parseDouble(et_valor2.getText().toString());
+                tv_resultado.setText(String.format("%.2f", (num1 / num2)));
             }
         });
     }//method
