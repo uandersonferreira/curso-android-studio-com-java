77a41bfa6cd737263932f1c80a4f6d48f3ca82d3
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-02-10 16:18:51 -0300

    manipulando valores da view e do código + checkBox simples

--- app/src/main/java/uanderson/com/br/curso/android_studio/MainActivity.java
+++ app/src/main/java/uanderson/com/br/curso/android_studio/MainActivity.java
@@ -2,85 +2,86 @@ package uanderson.com.br.curso.android_studio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private double num1 = 0, num2 = 0;
-    private Button btn_somar, btn_subtrair, btn_multiplicar, btn_dividir;
-    private TextView tv_resultado;
-    private EditText et_valor1, et_valor2;
+    private double valueTaca, valuePrato, valueGarfo, valueFaca, valueTotal;
+    private EditText editTextTaca, editTextPrato, editTextGarfo, editTextFaca;
+    private CheckBox checkBoxTaca, checkBoxPrato, checkBoxGarfo, checkBoxFaca;
+    private Button buttonCalcular;
+    private TextView textViewResultado;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//Relaciona o código a view/tela
 
         // Associando os elementos da view ao código java que irá manipular eles.
-        btn_somar = findViewById(R.id.btn_soma);
-        btn_subtrair = findViewById(R.id.btn_subtrai);
-        btn_multiplicar = findViewById(R.id.btn_multiplica);
-        btn_dividir = findViewById(R.id.btn_divide);
+        editTextTaca = findViewById(R.id.et_taca);
+        editTextPrato = findViewById(R.id.et_prato);
+        editTextGarfo = findViewById(R.id.et_garfo);
+        editTextFaca = findViewById(R.id.et_faca);
 
-        tv_resultado = findViewById(R.id.tv_resultado);
+        checkBoxTaca = findViewById(R.id.cb_tacas);
+        checkBoxPrato = findViewById(R.id.cb_pratos);
+        checkBoxGarfo = findViewById(R.id.cb_garfos);
+        checkBoxFaca = findViewById(R.id.cb_facas);
 
-        et_valor1 = findViewById(R.id.et_valor1);
-        et_valor2 = findViewById(R.id.et_valor2);
+        buttonCalcular = findViewById(R.id.btn_calcular);
+
+        textViewResultado = findViewById(R.id.tv_resultado);
+
+        //Associando manualmente valores a cada variavel dos "objeto"
+        valueTaca = 0.25;
+        valuePrato = 0.20;
+        valueGarfo = 0.15;
+        valueFaca = 0.15;
+        valueTotal = 0.0;
+
+
+        //Estou setando um novo valor: pego o valor atual do checkbox e concateno com o valor R$ do "objeto" - Taças: R$ + 0.25
+        checkBoxTaca.setText(checkBoxTaca.getText().toString() + valueTaca);
+        checkBoxPrato.setText(checkBoxPrato.getText().toString() + valuePrato);
+        checkBoxGarfo.setText(checkBoxGarfo.getText().toString() + valueGarfo);
+        checkBoxFaca.setText(checkBoxFaca.getText().toString() + valueFaca);
+
+        buttonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcucar();
+            }
+        });
 
     }//method onCreated
 
-    public void somar() {
-        num1 = Double.parseDouble(et_valor1.getText().toString());
-        num2 = Double.parseDouble(et_valor2.getText().toString());
-        tv_resultado.setText(String.format("%.2f", (num1 + num2)));
-    }
-    public void subtrair() {
-        num1 = Double.parseDouble(et_valor1.getText().toString());
-        num2 = Double.parseDouble(et_valor2.getText().toString());
-        tv_resultado.setText(String.format("%.2f", (num1 - num2)));
-    }
+    public void calcucar(){
+        valueTotal = 0.0;
 
-    public void multiplicar() {
-        num1 = Double.parseDouble(et_valor1.getText().toString());
-        num2 = Double.parseDouble(et_valor2.getText().toString());
-        tv_resultado.setText(String.format("%.2f", (num1 * num2)));
-    }
+        if (checkBoxTaca.isChecked()){
+            valueTotal += valueTaca * Double.parseDouble(editTextTaca.getText().toString());
+        }
+        if (checkBoxPrato.isChecked()) {
+            valueTotal += valuePrato * Double.parseDouble(editTextPrato.getText().toString());
+        }
+        if (checkBoxGarfo.isChecked()) {
+            valueTotal += valueGarfo * Double.parseDouble(editTextGarfo.getText().toString());
+        }
+        if (checkBoxFaca.isChecked()) {
+            valueTotal += valueFaca * Double.parseDouble(editTextFaca.getText().toString());
+        }
+        textViewResultado.setText("Valor total: R$ " + valueTotal);
 
-    public void dividir() {
-        num1 = Double.parseDouble(et_valor1.getText().toString());
-        num2 = Double.parseDouble(et_valor2.getText().toString());
-        tv_resultado.setText(String.format("%.2f", (num1 / num2)));
-    }
-
-    public void opera(View button){
-            if (button.getId() == R.id.btn_soma){
-                somar();
-            } else if (button.getId() == R.id.btn_subtrai) {
-                subtrair();
-            } else if (button.getId() == R.id.btn_multiplica) {
-                multiplicar();
-            }else {
-                dividir();
-            }
     }
     /*
-        Nesta abordagem criamos um method e associamos a todos os botões
-        da nossa view e através do "id" de cada um fazemos a verificação
-        para saber qual foi o clicado e então chamar o method correspondenete
-        a operação desejada. Pode ser utilizar o if else ou switch case.
+        Verificando manualmente cada um dos checkBox implementados e realizando o cálculo.
      */
-
 
 }//class
 
-/*
-TUDO está declarado/possui uma instância na class R
-
-R.  layout      .activity_main
-R,  categoria  , item que desejo ter acesso/utilizar
-Button - componente do tipo View
-
- */
