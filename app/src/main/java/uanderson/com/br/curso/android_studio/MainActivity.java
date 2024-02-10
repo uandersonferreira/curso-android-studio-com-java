5a8a1cb11af19c625675375c302ce25a869153bf
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-02-10 15:19:48 -0300

    associação buttons com 1 metehod só e varias operações

--- app/src/main/java/uanderson/com/br/curso/android_studio/MainActivity.java
+++ app/src/main/java/uanderson/com/br/curso/android_studio/MainActivity.java
@@ -2,7 +2,6 @@ package uanderson.com.br.curso.android_studio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//Relaciona o código a view/tela
 
-
         // Associando os elementos da view ao código java que irá manipular eles.
         btn_somar = findViewById(R.id.btn_soma);
         btn_subtrair = findViewById(R.id.btn_subtrai);
@@ -31,75 +29,49 @@ public class MainActivity extends AppCompatActivity {
 
         et_valor1 = findViewById(R.id.et_valor1);
         et_valor2 = findViewById(R.id.et_valor2);
-        /*
-            Fazendo a associação de eventos de cliques nos botões através do proprio .xml
-            android:onClick="dividir"
-            Creio que esteja depreciado, pois aprece riscado, mas não deixa de ser uma forma válida
-            de fazer a associação.
-         */
 
-
-/*        btn_somar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            @SuppressLint("DefaultLocale")
-            public void onClick(View v) {
-                num1 = Double.parseDouble(et_valor1.getText().toString());
-                num2 = Double.parseDouble(et_valor2.getText().toString());
-                tv_resultado.setText(String.format("%.2f", (num1 + num2)));
-            }
-        });
-        btn_subtrair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            @SuppressLint("DefaultLocale")
-            public void onClick(View v) {
-                num1 = Double.parseDouble(et_valor1.getText().toString());
-                num2 = Double.parseDouble(et_valor2.getText().toString());
-                tv_resultado.setText(String.format("%.2f", (num1 - num2)));
-            }
-        });
-        btn_multiplicar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            @SuppressLint("DefaultLocale")
-            public void onClick(View v) {
-                num1 = Double.parseDouble(et_valor1.getText().toString());
-                num2 = Double.parseDouble(et_valor2.getText().toString());
-                tv_resultado.setText(String.format("%.2f", (num1 * num2)));
-
-            }
-        });
-        btn_dividir.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("DefaultLocale")
-            @Override
-            public void onClick(View v) {
-                num1 = Double.parseDouble(et_valor1.getText().toString());
-                num2 = Double.parseDouble(et_valor2.getText().toString());
-                tv_resultado.setText(String.format("%.2f", (num1 / num2)));
-            }
-        });*/
     }//method onCreated
 
-    public void somar(View view) {
+    public void somar() {
         num1 = Double.parseDouble(et_valor1.getText().toString());
         num2 = Double.parseDouble(et_valor2.getText().toString());
         tv_resultado.setText(String.format("%.2f", (num1 + num2)));
     }
-    public void subtrair(View view) {
+    public void subtrair() {
         num1 = Double.parseDouble(et_valor1.getText().toString());
         num2 = Double.parseDouble(et_valor2.getText().toString());
         tv_resultado.setText(String.format("%.2f", (num1 - num2)));
     }
 
-    public void multiplicar(View view) {
+    public void multiplicar() {
         num1 = Double.parseDouble(et_valor1.getText().toString());
         num2 = Double.parseDouble(et_valor2.getText().toString());
         tv_resultado.setText(String.format("%.2f", (num1 * num2)));
     }
 
-    public void dividir(View view) {
+    public void dividir() {
         num1 = Double.parseDouble(et_valor1.getText().toString());
         num2 = Double.parseDouble(et_valor2.getText().toString());
         tv_resultado.setText(String.format("%.2f", (num1 / num2)));
     }
+
+    public void opera(View button){
+            if (button.getId() == R.id.btn_soma){
+                somar();
+            } else if (button.getId() == R.id.btn_subtrai) {
+                subtrair();
+            } else if (button.getId() == R.id.btn_multiplica) {
+                multiplicar();
+            }else {
+                dividir();
+            }
+    }
+    /*
+        Nesta abordagem criamos um method e associamos a todos os botões
+        da nossa view e através do "id" de cada um fazemos a verificação
+        para saber qual foi o clicado e então chamar o method correspondenete
+        a operação desejada. Pode ser utilizar o if else ou switch case.
+     */
 
 
 }//class
